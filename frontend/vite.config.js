7351c1c5f00ad92dcbab2de78450415f103fcbda
omar <omarkhaledahmedov7@gmail.com>
2024-12-25 22:53:36 +0100

    oo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000, // Increase the warning limit to 1000 KB
+  },
 })
